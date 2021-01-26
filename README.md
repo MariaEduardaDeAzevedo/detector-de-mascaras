ace08fdc0bd100d5f5ec57f014629d611509c383
MariaEduardaDeAzevedo <maria.silva@ccc.ufcg.edu.br>
2021-01-26 08:47:09 -0300

    Atualizando README.md e adicionando bip no codigo

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
 # Reconhecendo máscaras
 
-
-![](./assets/gif-mascara.gif)
+<div align="center" width=500>
+    <img src="./assets/gif-mascara.gif">
+</>
 
 Este repositório contém um script em Python3 que reconhece se um rosto está ou não portando uma máscara!
 
-O código utiliza da biblioteca OpenCV para o processamento das imagens e Scikitlearn para o treinamento do modelo que classifica um rosto que porta ou não uma máscara. 
+O código utiliza da biblioteca OpenCV para o processamento das imagens e scikit-learn para o treinamento do modelo que classifica um rosto que porta ou não uma máscara. 
 
+O conjunto de imagens utilizadas para o treinamento do modelo pode ser encontrada na pasta [imagens](./imagens) e foi retirado desse [link](https://github.com/sandeco/CanalSandeco/tree/master/Deep%20Learning%20s%C3%A9rie/%2316%20-%20Detectando%20Faces%20em%20v%C3%ADdeo%20com%20Deep%20Learning).
+
+
+#### Mais sobre o código
+
+Em [open_cam.py](./open_cam.py) temos um script que viabiliza que a webcam do seu computador seja iniciada. 
+
+Nesse script carregamos um dataframe partindo do conjunto de imagens que temos e treinamos um modelo [K-Nearest Neighbor](https://medium.com/brasil-ai/knn-k-nearest-neighbors-1-e140c82e9c4e) para classificar os rostos.
+
+Para o reconhecimento genérico das faces, utiliza-se o [CascadeClassifier](https://docs.opencv.org/3.4/db/d28/tutorial_cascade_classifier.html), já incluso dentro da biblioteca do OpenCV. De modo geral, esse método de treinamento utiliza de um arquivo .xml, que também já é incluso no pacote, para treinar um modelo que reconheça rostos de forma genérica, utilizando o método [Viola-Jones](https://en.wikipedia.org/wiki/Viola%E2%80%93Jones_object_detection_framework) e [AdaBoost](https://towardsdatascience.com/boosting-algorithm-adaboost-b6737a9ee60c) para o melhoramento do desempenho.
+
+O algoritmo de Machine Learning escolhido para classificação foi o K-Nearest Neighbor, pois foi o que apresentou melhor desempenho diante o conjunto de teste e de validação. A ideia é que, em um próximo experimento, esse mesmo código possa ser refeito utilizando uma CNN!
+
+#### Alguns links
+
+Aqui vou deixar uns links de referência com o que pesquisei sobre o assunto para desenvolver o código!
+
+- [Documentação do OpenCV](https://docs.opencv.org/master/)
+- [Documentação do scikit-learn](https://scikit-learn.org/stable/)
+- [Playlist de Python + OpenCV do canal Universo Discreto](https://www.youtube.com/watch?v=oAH_GJclePY&list=PL-t7zzWJWPtx3enns2ZAV6si2p9zGhZJX)
+- [Detectando Faces em Vídeo com Deep Learning (vídeo do dono do repositório de onde tirei o conjunto de dados)](https://www.youtube.com/watch?v=7Roghby6f24&t=461s)
+- [Reconhecimento de Faces | Redes Neurais em PyTorch | Programando em 10 minutos (Peixe Babel)](https://www.youtube.com/watch?v=s2Nr8nHK66c&t=903s)
--- open_cam.py
+++ open_cam.py
@@ -1,27 +1,29 @@
 import cv2 as cv
 import functions
 
-cam = cv.VideoCapture(0)
+cam = cv.VideoCapture(0) #Iniciando a WebCam
 file_name = "haarcascade_frontalface_alt2.xml"
-classifier = cv.CascadeClassifier(f"{cv.haarcascades}/{file_name}")
+classifier = cv.CascadeClassifier(f"{cv.haarcascades}/{file_name}") #Modelo para reconhecer faces
 
-dataframe = functions.load_dataframe()
+dataframe = functions.load_dataframe() #Carregando dataframe com as imagens para treinamento
 
-X_train, X_test, y_train, y_test = functions.train_test(dataframe)
-pca = functions.pca_model(X_train)
+X_train, X_test, y_train, y_test = functions.train_test(dataframe) #Dividindo conjuntos de treino e teste
+pca = functions.pca_model(X_train) #Modelo PCA para extração de features da imagem
 
-X_train = pca.transform(X_train)
-X_test = pca.transform(X_test)
+X_train = pca.transform(X_train) #Conjunto de treino com features extraídas
+X_test = pca.transform(X_test) #Conjunto de teste com features extraídas
 
-knn = functions.knn(X_train, y_train)
+knn = functions.knn(X_train, y_train) #Treinando modelo classificatório KNN
 
+#Rótulo das classificações
 label = {
     0: "Sem mascara",
     1: "Com mascara"
 }
 
+#Abrindo a webcam...
 while True:
-    status, frame = cam.read()
+    status, frame = cam.read() #Lendo a imagem e extraindo frame
 
     if not status:
         break
@@ -29,29 +31,33 @@ while True:
     if cv.waitKey(1) & 0xff == ord('q'):
         break
     
+    #Transformando a imagem em escala de cinza
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+    #Detectando faces na imagem
     faces = classifier.detectMultiScale(gray)
 
+    #Iterando nas faces encontradas
     for x,y,w,h in faces:
-        gray_face = gray[y:y+h, x:x+w]
+        gray_face = gray[y:y+h, x:x+w] #Recortand região da face
 
         if gray_face.shape[0] >= 200 and gray_face.shape[1] >= 200:
-            gray_face = cv.resize(gray_face, (160,160))
-            vector = pca.transform([gray_face.flatten()])
+            gray_face = cv.resize(gray_face, (160,160)) #Redimensionando
+            vector = pca.transform([gray_face.flatten()]) #Extraindo features da imagem
 
-            pred = knn.predict(vector)[0]
+            pred = knn.predict(vector)[0] #Classificando a imagem
             classification = label[pred]
 
+            #Desenhando retangulos em torno da face
             if pred == 0:
                 cv.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 3)
+                print("\a")
             elif pred == 1:
                 cv.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
-
+            
+            #Escrevendo classificação e quantidade de faces vistas
             cv.putText(frame, classification, (x - 20,y + h + 50), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2, cv.LINE_AA)
             cv.putText(frame, f"{len(faces)} rostos identificados",(20,20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2, cv.LINE_AA)
 
+    #Mostrando o frame
     cv.imshow("Cam", frame)
-
-
-    
